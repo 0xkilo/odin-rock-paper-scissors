635226bbc8d6ab26ebaf006a6d447de40c1f8886
0xkilo <lshaw8010@gmail.com>
2022-02-02 03:04:31 +0000

    Added basic function for Rock, Paper, Scissors

---
+++ script.js
@@ -0,0 +1,37 @@
+function computerPlay(){
+    const gameArray = ['Rock', 'Paper', 'Scissors']
+    let randomChoice = gameArray[Math.floor(Math.random()*gameArray.length)];
+
+    return(randomChoice);
+}
+
+function playRound(playerSelection, computerSelection){
+
+
+
+    if (playerSelection === computerSelection){
+        return('Its a tie! you both chose ' + playerSelection);
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return('You win! Rock beats Scissors');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return('You lose! Rock loses to Paper');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return('You win! Paper beats Rock');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return('You lose! Paper loses to Scissors');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return('You win! Scissors beats Paper');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return('You lose! Scissors loses to Rock');
+    } else{
+        return('Incorrect answer, try again.')
+    }
+}
+
+const computerSelection = computerPlay().toLowerCase()
+const playerSelection = prompt('Rock, Paper or Scissors?:').toLowerCase()
+
+console.log(playRound(playerSelection, computerSelection));
+
+function game()
+
